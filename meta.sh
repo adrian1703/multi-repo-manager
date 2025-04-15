e4686bee6d024d61dc2fb2a2c073e399420ebd2c
adrian <adrian.kuhn@arcor.de>
2025-04-15 20:00:01 +0200

    feat: add script to execute commands across repositories
    Signed-off-by: adrian <adrian.kuhn@arcor.de>

---
+++ meta.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+repos_file_location="$SCRIPT_DIR/repos.yaml"
+
+if [[ -z "$1" ]]; then
+  echo -e "${YELLOW}WARN: No command provided!${RESET}"
+  echo -e "${CYAN}DEFAULT:${RESET} ${GREEN}pwd${RESET}"
+  CMD="pwd"
+else
+  echo -e "${GREEN}Command provided: '$1'${RESET}"
+  CMD="$1"
+fi
+
+
+
+yq ".repos[].name" "$repos_file_location" |
+while read -r directory; do
+  cd "$SCRIPT_DIR/$directory" || {
+    echo -e "${RED}Failed to cd to $directory. Skipping...${RESET}"
+    continue
+  }
+  echo -e "${GREEN}Executing '${CMD}' in: $directory${RESET}"
+  eval "$CMD"
+done
